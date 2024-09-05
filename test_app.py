d8b0505e2f0ee387807c4b09e720d49257845ac8
iam-aredla <45419101+iam-aredla@users.noreply.github.com>
2024-09-05 18:03:05 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,13 +1,7 @@
-import unittest
+# test_app.py for pytest
 from app import app
 
-class BasicTests(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-
-    def test_home(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-
-if __name__ == "__main__":
-    unittest.main()
+def test_home():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
